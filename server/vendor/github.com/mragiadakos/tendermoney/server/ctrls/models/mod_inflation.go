a9403407a4da103310954bc269a9f8eb5bf1b787
manos <manos.ragiadakos@gmail.com>
2018-07-11 23:21:09 +0300

    Removing vendor

--- server/vendor/github.com/mragiadakos/tendermoney/server/ctrls/models/mod_inflation.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type InflationData struct {
-	Coin     string //uuid
-	Value    float64
-	Owner    string //public key hex
-	Inflator string //public key hex
-}
--- server/vendor/github.com/mragiadakos/tendermoney/server/ctrls/models/mod_sum.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type SumData struct {
-	Coins    []string //uuid
-	NewCoin  string   // signature hex
-	NewOwner string   //public key hex
-}
